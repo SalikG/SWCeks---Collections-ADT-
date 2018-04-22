3870f143bf49ae20b972e1e9c4d8c54908cc90de
Unknown <salikvilmer@gmail.com>
2018-04-22 18:00:21 +0200

    sagsd

---
+++ src/MainClass.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class MainClass {
+
+    public static void main(String[] args){
+        Role admin = new Role("admin");
+        Role secretary = new Role("secretary");
+        Role sales = new Role("sales");
+        Role normalUser = new Role("normal user");
+
+
+        User user1 = new User("Jens", 1111, new ArrayList<Role>(){{add(normalUser);}});
+        User user2 = new User("Peter", 1234, new ArrayList<Role>(){{add(sales); add(secretary);}});
+        User user3 = new User("Karlos", 3333, new ArrayList<Role>(){{add(sales);}});
+        User user4 = new User("Hans", 2346, new ArrayList<Role>(){{add(admin);}});
+
+        ArrayList<User> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+        try {
+            for (User u : users) {
+                System.out.print("Roles: ");
+                if (u.getRoles().contains(sales)) {
+                    System.out.print("sales   ");
+                }
+                Thread.sleep(700);
+                if (u.getRoles().contains(admin)) {
+                    System.out.print("admin   ");
+                }
+                Thread.sleep(700);
+                if (u.getRoles().contains(secretary)) {
+                    System.out.print("secretary   ");
+                }
+                Thread.sleep(700);
+                if (u.getRoles().contains(normalUser)) {
+                    System.out.print("normal user   ");
+                }
+                Thread.sleep(700);
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class User {
+    private String name;
+    private int password;
+    private ArrayList<Role> roles;
+
+    public User(String name, int password, ArrayList<Role> roles) {
+        this.name = name;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public ArrayList<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(ArrayList<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", password=" + password +
+                ", roles=" + roles +
+                '}';
+    }
+}
