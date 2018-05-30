e139662a8fa3353b0275dc15cb2dafed6909e90c
Unknown <salikvilmer@gmail.com>
2018-05-30 12:07:29 +0200

    sdgfds

---
+++ src/Streams.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Streams {
+    public static void main(String[] args) {
+
+        System.out.println(
+                IntStream
+                .range(1, 5)
+                .sum()
+        );
+
+        System.out.println();
+        System.out.println();
+
+        List<String> people = Arrays.asList("Peter", "Bo", "Ip", "Salik", "Gurli", "Allan", "Mester", "Sullivan");
+
+        people.stream()
+                .map(String::toLowerCase)
+                .filter(x -> x.startsWith("s"))
+                .forEach(System.out::println);
+    }
+}
