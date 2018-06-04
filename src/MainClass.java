2886d7ad02afd7f06fe49bc071fa3020529eafe7
Unknown <salikvilmer@gmail.com>
2018-06-04 17:33:22 +0200

    sdfg

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,43 @@
-public class MainClass {
-    public static void main(String[] args) {
+interface Inter{
+    void show();
+}
 
+// 1. måde
+class A implements Inter{
+    @Override
+    public void show() {
+        System.out.println("implementering i en klasse");
+    }
+}
+public class MainClass {
+
+
+    public static void main(String[] args) {
+        Inter obj;
+
+        // 1. måde
+        obj = new A();
+        obj.show();
+
+
+        // 2. måde
+        obj = new Inter() {
+            @Override
+            public void show() {
+                System.out.println("bla bla");
+            }
+        };
+        obj.show();
+
+
+        // 3. måde
+
+//        obj = () -> {
+//            System.out.println("lambda");
+//        };
+
+        obj = () -> System.out.println("lambda");
+
+        obj.show();
     }
 }
